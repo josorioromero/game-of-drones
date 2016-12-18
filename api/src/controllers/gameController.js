4f187ddb3c97097f2273f4da843e8b8956b39817
josorioromero <jorgeosorioromero@gmail.com>
2016-12-18 10:05:06 -0500

    Refactor on API project, added controller for games

---
+++ api/src/controllers/gameController.js
@@ -0,0 +1,58 @@
+import mongoose from 'mongoose';
+import Game from '../models/game.js';
+
+function getGames(req, res) {
+    Game.find((err, games) => {
+        if (err) {
+            res.send(err);
+        }
+        res.json(games);
+    });
+}
+
+function getGameById(req, res) {
+    Game.findById(req.params.game_id, (err, game) => {
+        if (err) {
+            res.send(err);
+        }
+        res.json(game);
+    });
+}
+
+function postGame(req, res) {
+    var newGame = new Game(req.body);
+
+    newGame.save(err => {
+        if (err) {
+            res.send(err);
+        }
+        res.json({message: 'Game created!'});
+    });
+}
+
+function updateGame(req, res) {
+    Game.findById({
+        _id: req.params.game_id
+    }, (err, game) => {
+        if (err)
+            res.send(err);
+        Object.assign(game, req.body).save((err, game) => {
+            if (err)
+                res.send(err);
+            res.json({message: 'Game updated!'});
+        });
+    });
+}
+
+function deleteGame(req, res) {
+    Game.remove({
+        _id: req.params.game_id
+    }, (err, user) => {
+        if (err) {
+            res.send(err);
+        }
+        res.json({message: 'Game successfully deleted'});
+    });
+}
+
+module.exports = { getGames, postGame, getGameById, updateGame, deleteGame };
--- api/src/server.js
+++ api/src/server.js
@@ -4,6 +4,7 @@ import bodyParser from 'body-parser';
 import morgan from 'morgan';
 import mongoose from 'mongoose';
 import Game from './models/game';
+import gameController from './controllers/gameController.js';
 
 //app configuration
 let app = express();
@@ -49,76 +50,31 @@ router.route('/game')
 
 //GET http://localhost:3000/api/game
 .get((req, res) => {
-    Game.find((err, games) => {
-      if (err) {
-        res.send(err);
-      }
-      res.json(games);
-    });
+  gameController.getGames(req, res);
 })
 
 //POST http://localhost:3000/api/game
 .post((req, res) => {
-    var game = new Game();
-    game.player1 = req.body.player1;
-    game.player2 = req.body.player2;
-    game.winner = req.body.winner;
-    game.rounds = req.body.rounds;
-
-    game.save(err => {
-        if (err) {
-            res.send(err);
-        }
-        res.json({message: 'Game created!'});
-    });
+  gameController.postGame(req, res);
 });
 //---------------------
-
 //Routing for /game/:game_id
 //-------------------------
 router.route('/game/:game_id')
 
 //GET http://localhost:3000/api/game/1
 .get((req, res) => {
-    Game.findById(req.params.game_id, (err, game) => {
-        if (err) {
-            res.send(err);
-        }
-        res.json(game);
-    });
+  gameController.getGameById(req, res);
 })
 
 //PUT http://localhost:3000/api/game/1
 .put((req, res) => {
-    Game.findById(req.params.game_id, (err, game) => {
-        if (err) {
-            res.send(err);
-        }
-
-        game.player1 = req.body.player1;
-        game.player2 = req.body.player2;
-        game.winner = req.body.winner;
-        game.rounds = req.body.rounds;
-
-        game.save((err) => {
-            if (err) {
-                res.send(err);
-            }
-            res.json({message: 'Game updated'});
-        });
-    });
+  gameController.updateGame(req, res);
 })
 
 //DELETE http://localhost:3000/api/game/1
 .delete((req, res) => {
-    Game.remove({
-        _id: req.params.game_id
-    }, (err, user) => {
-        if (err) {
-            res.send(err);
-        }
-        res.json({message: 'Game successfully deleted'});
-    });
+  gameController.deleteGame(req, res);
 });
 //-------------------------
 
