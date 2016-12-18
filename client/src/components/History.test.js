08a38a60cf4e11dde7c5624aec8b22319adbf0b9
josorioromero <jorgeosorioromero@gmail.com>
2016-12-18 10:09:32 -0500

    Added render testing for Header and History component

---
+++ client/src/components/History.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import History from './History';
+
+it('History renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<History />, div);
+});
